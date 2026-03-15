560753c40e179cae048e2627f08d98c4848b6ccd
gubeseju-ctrl <gubeseju@gmail.com>
2026-03-15 15:08:14 -0500

    primer commit de mi calculadora

---
+++ main.py
@@ -0,0 +1,19 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+operacion = input("Digite la operación (+, -, *, /): ")
+
+def calcular(num1, num2, operacion):
+    if operacion == "+":
+        return num1 + num2
+    elif operacion == "-":
+        return num1 - num2
+    elif operacion == "*":
+        return num1 * num2
+    elif operacion == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: División por cero"
+    else:
+        return "Operación no válida"
